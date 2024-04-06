214d62afb77ee8f99c096352103743ef6849a77d
AlessandroLeanza <aleleanza91@live.it>
2024-04-06 14:38:51 +0200

    Apportate modifiche generali + prezzo finale di due colori

--- js/apps.js
+++ js/apps.js
@@ -1,18 +1,23 @@
 console.log("Bootstrap-Preventivo")
-
+// creo un array di stringhe con i codici sconto
 const codSconto = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+// mi recupero dal dom l'id dove inserire i codici sconto
 const codiceInserito = document.getElementById('CodProm');
 
-// recupero dal document i tipi di lavoro
-const form = document.getElementById('tipo-di-lavoro')
+// recupero dal dom il form
+const form = document.getElementById('form')
+// quando premo il bottone scateno un evento di tipo lavoro
 form.addEventListener('submit', tipoLavoro)
-
+// recupero l'id prezzo dove andare ad inserire il prezzo finale
 prezzoElement = document.getElementById('prezzo')
 
+// creo una funzione che in base al tipo di lavoro selezionato di assegna un valore
 function tipoLavoro(e) {
+    // sto modificando il comportamnto di defoult del tag form con all'interno il buttom
     e.preventDefault()
+    // inseriamo i valori nel document
     const job = document.getElementById('job').value
-    // creiamo un controllo ai tipi di lavoro e inseriamo i valori nel document
+    // creo un controllo ai vari tipi di lavoro 
     let prezzo = 0;
     if (job === 'Backed Development') {
         prezzo = 20.50 * 10
@@ -21,7 +26,9 @@ function tipoLavoro(e) {
     } else if (job === 'Project Analysis') {
         prezzo = 33.60 * 10
     }
+    // assegno alla variabile sconto il valore in base al codice inserito
     const sconto = codiceInserito.value;
+    // creo un controllo per vedere se il codice sconto è incluso tra quelli disponibili
     const controllo = codSconto.includes(sconto)
     if (controllo) {
         prezzo = prezzo * 0.75;
@@ -31,7 +38,14 @@ function tipoLavoro(e) {
     } else if (!controllo) {
         alert('codice non valido')
     }
-    prezzo = prezzo.toFixed(2)
-    prezzoElement.innerHTML = "&euro; " + prezzo;
+
+    // Formatto il prezzo finale
+    const prezzoIntero = Math.floor(prezzo);
+    const prezzoDecimale = (prezzo % 1).toFixed(2);
+
+    // Mi vado a stampare sul documento il prezzo finale con la parte intera e decimale separate dalla virgola
+    document.querySelector('#prezzo .intero').innerHTML = prezzoIntero;
+    document.querySelector('#prezzo .decimal').innerHTML = prezzoDecimale.slice(2);
+    document.querySelector('#prezzo .virgola').innerHTML = ',';
 }
 
